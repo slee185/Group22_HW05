4c7b382edd9426e53cdaab2ab49178d8f0139ca4
Stephanie <slee185@uncc.edu>
2022-09-28 14:02:40 -0400

    implement threadpool

--- app/src/main/java/com/example/group22_hw05/MainActivity.java
+++ app/src/main/java/com/example/group22_hw05/MainActivity.java
@@ -5,18 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class MainActivity extends AppCompatActivity {
+
+    Executor threadPool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.buttonGenerate).setOnClickListener(v -> {
-            Thread thread = new Thread(new DoWork(), "Worker 1");
-            thread.start();
+        threadPool = Executors.newFixedThreadPool(2);
 
-        });
+        findViewById(R.id.buttonGenerate).setOnClickListener(v -> threadPool.execute(new DoWork()));
     }
 
     class DoWork implements Runnable {
