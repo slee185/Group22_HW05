2b2f3222c45c977bb8ef9ce534e588029998353f
Stephanie <slee185@uncc.edu>
2022-09-28 13:57:45 -0400

    basic thread code started

--- app/src/main/java/com/example/group22_hw05/MainActivity.java
+++ app/src/main/java/com/example/group22_hw05/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.group22_hw05;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.buttonGenerate).setOnClickListener(v -> {
+            Thread thread = new Thread(new DoWork(), "Worker 1");
+            thread.start();
+
+        });
+    }
+
+    class DoWork implements Runnable {
+
+        @Override
+        public void run() {
+
+        }
     }
 }
