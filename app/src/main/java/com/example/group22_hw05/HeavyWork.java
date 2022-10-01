32a1478eb51753a34cf4f452d329d5225a59608e
Ken Stanley <dohpaz@gmail.com>
2022-10-01 16:38:16 -0400

    Format files

--- app/src/main/java/com/example/group22_hw05/HeavyWork.java
+++ app/src/main/java/com/example/group22_hw05/HeavyWork.java
@@ -8,19 +8,19 @@ import java.util.Random;
 
 
 public class HeavyWork {
-	public static final long DELAY_MILLI_SECS = 2000;
+    public static final long DELAY_MILLI_SECS = 2000;
 
-	public static double getNumber(){
-		addSomeDelay(DELAY_MILLI_SECS);
-		Random rand = new Random();
-		return rand.nextDouble();
-	}
+    public static double getNumber() {
+        addSomeDelay(DELAY_MILLI_SECS);
+        Random rand = new Random();
+        return rand.nextDouble();
+    }
 
-	private static void addSomeDelay(long millis){
-		try {
-			Thread.sleep(millis);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+    private static void addSomeDelay(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- app/src/main/java/com/example/group22_hw05/MainActivity.java
+++ app/src/main/java/com/example/group22_hw05/MainActivity.java
@@ -7,7 +7,6 @@ package com.example.group22_hw05;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -15,6 +14,7 @@ import java.util.concurrent.Executors;
 public class MainActivity extends AppCompatActivity {
 
     Executor threadPool;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.buttonGenerate).setOnClickListener(v -> threadPool.execute(new DoWork()));
     }
 
-    class DoWork implements Runnable {
+    static class DoWork implements Runnable {
 
         @Override
         public void run() {
