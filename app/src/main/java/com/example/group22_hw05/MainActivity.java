409ad1948927353c21d8d7e97e01755a88d3d44f
Ken Stanley <dohpaz@gmail.com>
2022-10-01 16:56:36 -0400

    Enable view bindings

--- app/src/main/java/com/example/group22_hw05/MainActivity.java
+++ app/src/main/java/com/example/group22_hw05/MainActivity.java
@@ -8,21 +8,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.group22_hw05.databinding.ActivityMainBinding;
+
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     Executor threadPool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         threadPool = Executors.newFixedThreadPool(2);
-
-        findViewById(R.id.buttonGenerate).setOnClickListener(v -> threadPool.execute(new DoWork()));
+        binding.buttonGenerate.setOnClickListener(v -> threadPool.execute(new DoWork()));
     }
 
     static class DoWork implements Runnable {
